779cea4a1f6a27f48efd7a66500ca3ffb3606d8c
ISHEET SHETTY <isheetshetty@gmail.com>
2020-09-14 00:44:33 +0530

    INNER FOLDER VCET LOGO CHANGES  ISHEET

--- Admin/Students/Index.php
+++ Admin/Students/Index.php
@@ -15,7 +15,7 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <img src="Images/vcetlogoicon.png"></img>&emsp;
+        <img src="../../Images/vcetlogoicon.png"></img>&emsp;
         <a class="navbar-brand" href="https://vcet.edu.in/">VCET</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
