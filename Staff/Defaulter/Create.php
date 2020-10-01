59bed5d7d681c75e4ea3688e3bfe59841c4f7ec3
yash0201 <56736232+yash0201@users.noreply.github.com>
2020-10-02 04:38:54 +0530

    Defaulter Create cookies

---
+++ Staff/Defaulter/Create.php
@@ -0,0 +1,15 @@
+<?php 
+    $StaffId = $_COOKIE["StaffId"];
+    $SubjectId = $_COOKIE["SubjectId"];
+    $AcademicSessionId = $_COOKIE["AcademicSessionId"];
+
+    $servername="localhost";
+    $username="root";
+    $password="";
+    $db="vceterp";
+    $con = new mysqli($servername,$username,$password,$db);
+    if(!$con)
+    {
+        die('could not connect'.mysql_error());
+    }    
+?>
