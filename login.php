4f0d50bc48de1f1f651461fd5c68d861debe385d
ISHEET SHETTY <isheetshetty@gmail.com>
2020-10-03 04:25:40 +0530

    nav login

--- login.php
+++ login.php
@@ -34,38 +34,10 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <!-- <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li> -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Institute Management
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="Admin/Branch/Index.php">Branch Master</a>
-                        <a class="dropdown-item" href="#">Academic Session</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Assign Staff</a>
-                        <a class="dropdown-item" href="#">Assign Student</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Branch Management
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <!-- <div class="dropdown-divider"></div> -->
-                        <a class="dropdown-item" href="Admin/Staff/Index.php">Staff Master</a>
-                        <a class="dropdown-item" href="Admin/Subject/Index.php">Subject Master</a>
-                        <a class="dropdown-item" href="Admin/Students/Index.php">Student Master</a>
-                    </div>
-                </li>                
+               
             </ul>
             <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                
             </form>
         </div>
     </nav>
