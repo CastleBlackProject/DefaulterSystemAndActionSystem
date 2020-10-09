1ba26378fdd907db45bb3fc1048e3ac57dc0d028
yash0201 <56736232+yash0201@users.noreply.github.com>
2020-10-09 23:15:51 +0530

    Name Check Done

--- Staff/Lecture/OnlineLecture.js
+++ Staff/Lecture/OnlineLecture.js
@@ -1,11 +1,3 @@
-// $("#btn_MeetAttendance").click(function(){
-//     var RawData = $("#txt_Attendance").val();
-//     console.log(RawData);
-// });
-
-//alert("Online");
-
-
 var unmarkedRollNo = [];
 
 function takeRawData2() {
@@ -13,142 +5,121 @@ function takeRawData2() {
     processData2(RawData);
 }
 
-// function processData2(RawData) {
-
-//     var arr_RawData = RawData.split(' ');
-
-//     //console.log("Length : " + arr_RawData.length);
-
-//     for (var i = 0; i < arr_RawData.length; i++) {
-
-//         //console.log(arr_RawData[i]);
-
-//         if (i % 2 == 0 && i != 0) {
-
-//             var finalraw = arr_RawData[i].split("\n"); // AM/PM, TE-IT-47, First Name
-//             var arr_RollNo = finalraw[1].split("-");
-//             //console.log(arr_RollNo[2]);
-
-
-//             if (i == 2) {
-//                 var firstname = arr_RawData[0];
-//                 var lastnameraw = arr_RawData[i - 1];
-//             }
-//             else {
-//                 var firstnameraw = arr_RawData[i - 2].split("\n")
-//                 var firstname = firstnameraw[2];
-//                 var lastnameraw = arr_RawData[i - 1];
-//             }
-
-//             var lastname = "";
-
-//             for (var k = 0; k < lastnameraw.length; k++) {
-
-//                 var t = parseInt(lastnameraw[k]);
-//                 if (t <= 9 && t >= 0) {
-//                     break;
-//                 }
-//                 else {
-//                     lastname += lastnameraw[k];
-//                 }
-//             }
-
-//             var fullname = firstname + " " + lastname;
-
-//             outputData2(arr_RollNo[2], fullname);
-//         }
-//     }
-
-//     if (unmarkedRollNo.length != 0) {
-//         for (var i = 0; i < unmarkedRollNo.length; i++) {
-//             console.log(unmarkedRollNo[i]);
-//         }
-//     }
-// }
-
-
 function processData2(RawData) {
 
     var arr_RawData = RawData.split('\n');
 
-    for (var i = 0; i < arr_RawData.length; i++) {
-        console.log(arr_RawData[i]);
+    for (var i = 0; i < arr_RawData.length; i = i + 2) {
+
+        var raw_Name_Time = arr_RawData[i]; // Name & Time
+        var chat = arr_RawData[i + 1]; // TE-IT-RollNo
+
+        //console.log(chat);
+
+        var Name = "";
+
+        for (var j = 0; j < raw_Name_Time.length; j++) {
+
+            var x= parseInt(raw_Name_Time[j]);
+            if (x <= 9 && x >= 0) {
+                break;
+            }
+            else {
+                Name += raw_Name_Time[j];
+            }            
+        }
+
+        var rawRollNo = chat.split("-");
+        
+        if(rawRollNo[2] != null){
+            if(rawRollNo[2] > 0){
+                var RollNo = rawRollNo[2];
+                outputData2(RollNo, Name);
+            }            
+        }
+        
     }
-
-
-    //console.log("Length : " + arr_RawData.length);
-
-    // for (var i = 0; i < arr_RawData.length; i++) {
-
-    //     //console.log(arr_RawData[i]);
-
-    //     if (i % 2 == 0 && i != 0) {
-
-    //         var finalraw = arr_RawData[i].split("\n"); // AM/PM, TE-IT-47, First Name
-    //         var arr_RollNo = finalraw[1].split("-");
-    //         //console.log(arr_RollNo[2]);
-
-
-    //         if (i == 2) {
-    //             var firstname = arr_RawData[0];
-    //             var lastnameraw = arr_RawData[i - 1];
-    //         }
-    //         else {
-    //             var firstnameraw = arr_RawData[i - 2].split("\n")
-    //             var firstname = firstnameraw[2];
-    //             var lastnameraw = arr_RawData[i - 1];
-    //         }
-
-    //         var lastname = "";
-
-    //         for (var k = 0; k < lastnameraw.length; k++) {
-
-    //             var t = parseInt(lastnameraw[k]);
-    //             if (t <= 9 && t >= 0) {
-    //                 break;
-    //             }
-    //             else {
-    //                 lastname += lastnameraw[k];
-    //             }
-    //         }
-
-    //         var fullname = firstname + " " + lastname;
-
-    //         outputData2(arr_RollNo[2], fullname);
-    //     }
-    // }
-
-    // if (unmarkedRollNo.length != 0) {
-    //     for (var i = 0; i < unmarkedRollNo.length; i++) {
-    //         console.log(unmarkedRollNo[i]);
-    //     }
-    // }
 }
 
-// function outputData2(RollNo, Name) {
+function outputData2(RollNo, Name) {
 
-//     console.log(Name + "-" + RollNo);
+    //console.log(Name + "-" + RollNo);    
 
-//     var name = Name.split(" ");
+    var tbody = document.getElementById("tbody_Students");
+    var countRows = $('#table_Students tbody').find('tr').length;
 
-//     var tbody = document.getElementById("tbody_Students");
-//     var countRows = $('#table_Students tbody').find('tr').length;
+    var isMarked = false;
 
-//     var isMarked = false;
+    for (var i = 1; i <= countRows; i++) {
+        var row = tbody.childNodes[i];
+        var chkbox = row.childNodes[3].childNodes[0]
+        var table_RollNo = row.childNodes[3].childNodes[0].value;
 
-//     for (var i = 1; i <= countRows; i++) {
-//         var row = tbody.childNodes[i];
-//         var chkbox = row.childNodes[3].childNodes[0]
-//         var table_RollNo = row.childNodes[3].childNodes[0].value;
+        if (table_RollNo == RollNo) {
 
-//         if (table_RollNo == RollNo) {
-//             $(chkbox).prop('checked', true);
-//             document.getElementById("btn_closeModal").click();
-//             isMarked = true;
-//         }
-//     }
+            var table_Name = row.childNodes[2].innerHTML;
+            
+            var table_Names = table_Name.split(" ");
+            if(table_Names.length == 2){
+                var name = Name.split(" ");
+                if(name.length == 2){
+                    var isPresent = false;
+                    if(name[0].toUpperCase() === table_Names[0].toUpperCase() && name[1].toUpperCase() === table_Names[1].toUpperCase()){
+                        var isPresent = true;
+                    }
+                    if(!isPresent){
+                        if(name[0].toUpperCase() === table_Names[1].toUpperCase() && name[1].toUpperCase() === table_Names[0].toUpperCase()){
+                            var isPresent = true;
+                        }
+                    }
+                }
+                else if(name.length == 3){
+                    var isPresent = false;
+                    if(name[0].toUpperCase() === table_Names[0].toUpperCase() && name[2].toUpperCase() === table_Names[1].toUpperCase()){
+                        var isPresent = true;
+                    }
+                    if(!isPresent){
+                        if(name[0].toUpperCase() === table_Names[1].toUpperCase() && name[2].toUpperCase() === table_Names[0].toUpperCase()){
+                            var isPresent = true;
+                        }
+                    }
+                }
+            }
+            else if(table_Names.length == 3){
+                var name = Name.split(" ");
+                if(name.length == 2){
+                    var isPresent = false;
+                    if(name[0].toUpperCase() === table_Names[0].toUpperCase() && name[1].toUpperCase() === table_Names[2].toUpperCase()){
+                        var isPresent = true;
+                    }
+                    if(!isPresent){
+                        if(name[0].toUpperCase() === table_Names[2].toUpperCase() && name[1].toUpperCase() === table_Names[0].toUpperCase()){
+                            var isPresent = true;
+                        }
+                    }
+                }
+                else if(name.length == 3){
+                    var isPresent = false;
+                    if(name[0].toUpperCase() === table_Names[0].toUpperCase() && name[2].toUpperCase() === table_Names[2].toUpperCase()){
+                        var isPresent = true;
+                    }
+                    if(!isPresent){
+                        if(name[0].toUpperCase() === table_Names[2].toUpperCase() && name[2].toUpperCase() === table_Names[0].toUpperCase()){
+                            var isPresent = true;
+                        }
+                    }
+                }
+            }
+            
+            if(isPresent){
+                $(chkbox).prop('checked', true);
+            document.getElementById("btn_closeModal").click();
+            isMarked = true;
+            }
+        }
+    }
 
-//     if (!isMarked) {
-//         unmarkedRollNo.push(RollNo);
-//     }
-// }
+    if (!isMarked) {
+        unmarkedRollNo.push(RollNo);
+    }
+}
