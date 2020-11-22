bfc7d1cd3c34d4d0e78cd487245db193b8078c19
yash0201 <56736232+yash0201@users.noreply.github.com>
2020-11-22 22:04:11 +0530

    Connection

--- checkLogin.php
+++ checkLogin.php
@@ -1,6 +1,6 @@
 <?php
 
-require '../../connection.php';
+require 'connection.php';
 // $servername="localhost";
 // $username="root";
 // $password="";
--- connection.php
+++ connection.php
@@ -6,7 +6,7 @@
     $con = new mysqli($servername,$username,$password,$db);
     if(!$con)
     {
-        die('could not connect'.mysql_error());
+        //die('could not connect'.mysql_error());
     }
     else
     {
